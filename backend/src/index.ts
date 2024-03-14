06ced3db28f38222f0b342df6116f6b92ff78bc6
Bhupen Gupta <bhupengupta05@gmail.com>
2024-03-14 22:50:06 +0530

    changed the port to 3000, used cors and frontend works

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,17 @@
 import express from 'express';
+import cors from 'cors'
+
 import diagnosisRouter from './routes/diagnosis'
 import patientsRouter from './routes/patients'
+
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 const PORT = 3000;
 
-app.get('/ping', (_req, res) => {
+// use /api before ping so no error comes
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
