a9baff53a461f6dee3e0e8a04f74174bfd970630
Bhupen Gupta <bhupengupta05@gmail.com>
2024-03-18 13:18:16 +0530

    Entries field is populated by using types in backend

--- backend/src/types/Patient/types.ts
+++ backend/src/types/Patient/types.ts
@@ -1,6 +1,42 @@
-export interface Entry {
+import { DiagnosisEntry } from "../Diagnosis/types"
 
+export interface BaseEntry {
+    id: string,
+    date: string,
+    description: string,
+    specialist: string,
+    diagnosisCodes?: Array<DiagnosisEntry['code']>
 }
+
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+}
+export interface HealthCheckEntry extends BaseEntry {
+    healthCheckRating: HealthCheckRating,
+    type: "HealthCheck"
+}
+
+export interface HospitalEntry extends BaseEntry {
+    discharge: {
+        date: string,
+        criteria: string
+    }
+    type: "Hospital"
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+    employerName: string,
+    sickLeave: {
+        startDate: string,
+        endDate: string,
+    },
+    type: "OccupationalHealthcare"
+}
+
+export type Entry = HospitalEntry | OccupationalHealthcareEntry | HealthCheckEntry
 
 export enum Gender {
     Male = 'male',
@@ -21,3 +57,4 @@ export interface PatientEntry {
     occupation: string,
     entries: Entry[]
 }
+
--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -1,4 +1,4 @@
-import { Gender, NewPatientEntry } from "./types/Patient/types"
+import { Entry, Gender, NewPatientEntry } from "./types/Patient/types"
 
 const isString = (text: unknown): text is string => {
     return typeof text === 'string' || text instanceof String
@@ -46,20 +46,36 @@ const parseGender = (gender: unknown): Gender => {
     }
     return gender
 }
+  
+// It worked
+const parseEntries = (entries: any): Entry[] => {
+    return entries.map((entry: any) => {
+        switch(entry.type) {
+            case 'Hospital':
+                return {id: entry.id, date: entry.date, description: entry.description, specialist: entry.specialist, diagnosisCode: entry.diagnosisCodes, discharge: entry.discharge, type: entry.type}
+            case 'OccupationalHealthcare':
+                return {id: entry.id, date: entry.date, description: entry.description, specialist: entry.specialist, diagnosisCode: entry.diagnosisCodes, employerName: entry.employerName, sickLeave: entry.sickLeave, type: entry.type}
+            case 'HealthCheck':
+                return {id: entry.id, date: entry.date, description: entry.description, specialist: entry.specialist, diagnosisCode: entry.diagnosisCodes, healthCheckRating: entry.healthCheckRating, type: entry.type}
+            default:
+                throw new Error(`Invalid entry type: ${entry.type}`)
+        }
+    })
+}
 
 const toNewPatientEntry = (object: unknown): NewPatientEntry => {
     if ( !object || typeof object !== 'object' ) {
         throw new Error('Incorrect or missing data')
     }
 
-    if ('name' in object && 'dateOfBirth' in object && 'gender' in object && 'occupation' in object && 'ssn' in object) {
+    if ('name' in object && 'dateOfBirth' in object && 'gender' in object && 'occupation' in object && 'ssn' in object && 'entries' in object) {
         const newEntry: NewPatientEntry = {
             name: parseName(object.name),
             dateOfBirth: parseDateOfBirth(object.dateOfBirth),
             gender: parseGender(object.gender),
             occupation: parseOccupation(object.occupation),
             ssn: parseSSN(object.ssn),
-            entries: [] // Assigning an empty array to entries
+            entries: parseEntries(object.entries) 
         }
         return newEntry
     }
